b9134b1f8bc23ffe99852c7b63fb7f8997886ff6
Sachin <rawatsachin8923@gmail.com>
2026-02-14 13:47:39 +0530

    Adding Season change page

---
+++ 1.4 Season Switcher Page/index.js
@@ -0,0 +1,22 @@
+let seasonSmallImageElement = document.getElementById("seasonSmallImage");
+let seasonMediumImageElement = document.getElementById("seasonMediumImage");
+
+function imageChangeIntoSpring() {
+  seasonSmallImageElement.src = "./seasons-switcher-spring-xs-img.png";
+  seasonMediumImageElement.src = "./seasons-switcher-spring-md-img.png";
+}
+
+function imageChangeIntoSummer() {
+  seasonSmallImageElement.src = "./seasons-switcher-summer-xs-img.png";
+  seasonMediumImageElement.src = "./seasons-switcher-summer-md-img.png";
+}
+
+function imageChangeIntoAutumn() {
+  seasonSmallImageElement.src = "./seasons-switcher-autumn-xs-img.png";
+  seasonMediumImageElement.src = "./seasons-switcher-autumn-md-img.png";
+}
+
+function imageChangeIntoWinter() {
+  seasonSmallImageElement.src = "./seasons-switcher-winter-xs-img.png";
+  seasonMediumImageElement.src = "./seasons-switcher-winter-md-img.png";
+}
